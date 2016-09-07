aa7a80c5fb6efa341a69814252264c0506dee177
Tobias Schmitt <tobias.schmitt@mni.thm.de>
2016-09-07 12:03:58 +0200

    save logs script updated

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 make clean
 make
 cp llearnd /usr/local/bin/llearnd
-mkdir -p /var/logs/llearnd
+mkdir -p /var/log/llearnd
---
+++ snu-log.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rsync -az /tmp/llearn/ /var/log/llearnd/
+# TODO: upload to webserver
