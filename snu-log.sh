dc7b12cbec0c8d4a893fa35e33092e40770cdcd9
Tobias Schmitt <tobias.schmitt@mni.thm.de>
2016-11-13 23:36:13 +0100

    change to ip

--- snu-log.sh
+++ snu-log.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rsync -az /tmp/llearn/ /var/log/llearnd/
-rsync  -az /var/log/llearnd/ ts@rhea.local:/home/ts/logs/
+rsync  -az /var/log/llearnd/ ts@192.168.34.5:/home/ts/logs/
 # TODO: upload to webserver
